8696e4159bc4b69a064c53820e5547e4ae47ffdc
V3506 (Conference ID) <v3506@cam.ac.uk>
2015-07-21 12:10:24 +0100

    before lunch day 2

--- js/flappytest.js
+++ js/flappytest.js
@@ -1,73 +1,46 @@
-// the functions associated with preload, create and update.
+/**
+ * Created by v3506 on 21/07/2015.
+ */
 var actions = { preload: preload, create: create, update: update };
-// the Game object used by the phaser.io library
 var width = 700;
 var height = 400;
 var game = new Phaser.Game(width, height, Phaser.AUTO, "game", actions);
-// Global score variable initialised to 0.
 var score = 0;
-// Global variable to hold the text displaying the score.
 var labelScore;
-// Global player variable declared but not initialised.
 var player;
-// Global pipes variable initialised to the empty array.
 var pipes = [];
-// the interval (in seconds) at which new pipe columns are spawned
-var gameGravity = 500;
+var gameGravity = 200;
 var gameSpeed = 200;
 var jumpPower = 200;
 var pipeInterval = 1.75;
 var pipeGap = 100;
+
+// Global variables to store the bonuses
 var balloons = [];
 var weights = [];
-var pipeWidth=50;
-var bonusWidth=50;
 
-// Loads all resources for the game and gives them names.
 function preload() {
-    // make image file available to game and associate with alias playerImg
-    game.load.image("playerImg", "../assets/flappy.png");
-    // make sound file available to game and associate with alias score
+    game.load.image("playerImg","../assets/flappy-cropped.png");
     game.load.audio("score", "../assets/point.ogg");
-    // make image file available to game and associate with alias pipe
-    game.load.image("pipe","../assets/pipe2-body.png");
-    game.load.image("pipeEnd","../assets/pipe2-end.png");
-    // make image file available to game and associate with alias BG (background)
-    game.load.image("background","../assets/SKY.png");
+    game.load.image("pipe","../assets/pipe.png");
+    game.load.image("pipeEnd","../assets/pipe-end.png");
     game.load.image("balloons","../assets/balloons.png");
     game.load.image("weight","../assets/weight.png");
-    game.load.image("star","..assets/star.png")
 }
 
-// Initialises the game. This function is only called once.
 function create() {
-    // set the background colour of the scene
-    game.stage.setBackgroundColor("#F3D3A3");
-    game.add.sprite(0,0, "background");
-    // add welcome text
-    game.add.text(20, 20, "...",
-        {font: "30px Arial", fill: "#000000"});
-    // add score text
+    game.stage.setBackgroundColor("#BADA55");
     labelScore = game.add.text(20, 60, "0",
-        {font: "30px Arial", fill: "#000000"});
-    // initialise the player and associate it with playerImg
+        {font: "30px Arial", fill: "#FFFFFF"});
     player = game.add.sprite(80, 200, "playerImg");
-   // player.scale.setTo(0.25, 0.25);
     player.anchor.setTo(0.5, 0.5);
-    // Start the ARCADE physics engine.
-    // ARCADE is the most basic physics engine in Phaser.
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    // enable physics for the player sprite
     game.physics.arcade.enable(player);
-    // set the player's gravity
     player.body.gravity.y = gameGravity;
-    // associate spacebar with jump function
     game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
-    // time loop for game to update
     game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generate);
 }
 
-// This function updates the scene. It is called for every new frame.
 function update() {
     game.physics.arcade.overlap(player, pipes, gameOver);
     if(0 > player.body.y || player.body.y > width){
@@ -79,17 +52,6 @@ function update() {
     // Check if the player gets a bonus
     checkBonus(balloons, -50);
     checkBonus(weights, 50);
-    // Clean the pipe array
-    for(var i=pipes.length - 1; i >= 0; i--) {
-        if(pipes[i].body.x - pipeWidth < 0){
-            pipes[i].destroy;
-            changeScore();
-            pipes.splice(i,1);
-        }
-    }
-    // Call gameOver function when player overlaps with pipe
-    // (i.e. when player hits a pipe)
-    game.physics.arcade.overlap(player, pipes, gameOver);
 }
 
 function checkBonus(bonusArray, bonusEffect) {
@@ -106,17 +68,11 @@ function checkBonus(bonusArray, bonusEffect) {
     }
 }
 
-// Adds a pipe part to the pipes array
 function addPipeBlock(x, y) {
-    // make a new pipe block
     var block = game.add.sprite(x, y, "pipe");
-    // insert it in the pipe array
     pipes.push(block);
-    // enable physics engine for the block
     game.physics.arcade.enable(block);
-    // set the block's horizontal velocity to a negative value
-    // (negative x value for velocity means movement will be towards left)
-    block.body.velocity.x = -200;
+    block.body.velocity.x = -gameSpeed;
 }
 
 function addPipeEnd(x, y) {
@@ -140,16 +96,15 @@ function generate(){
 function generatePipe() {
     var gapStart = game.rnd.integerInRange(50, height - 50 - pipeGap);
 
-    addPipeEnd(width-2,gapStart - 25);
+    addPipeEnd(width-5,gapStart - 25);
     for(var y=gapStart - 75; y>-50; y -= 50){
         addPipeBlock(width,y);
     }
-
-    addPipeEnd(width-2,gapStart+pipeGap+13);
+    addPipeEnd(width-5,gapStart+pipeGap);
     for(var y=gapStart + pipeGap + 25; y<height; y += 50){
         addPipeBlock(width,y);
     }
-    //changeScore();
+    changeScore();
 }
 
 function generateBalloons(){
@@ -178,7 +133,7 @@ function playerJump() {
 }
 
 function changeScore() {
-    score = score + 0.125;
+    score++;
     labelScore.setText(score.toString());
 }
 
